3dad97372da9f53e4e85ba524746b5de01f3eeec
JoelS-1 <82602257+JoelS-1@users.noreply.github.com>
2021-06-03 13:55:06 -0400

    initial scaffold commit

---
+++ AgileProject_MovieRater/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AgileProject_MovieRater.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AgileProject_MovieRater/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace AgileProject_MovieRater.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
